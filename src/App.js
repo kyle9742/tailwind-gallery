502fe6669310ea63b55dc9dde9a657414ca01472
it <kyle9742@naver.com>
2024-06-13 11:18:52 +0900

    이미지 없을 경우 text searchbar 밑으로 이동

--- src/App.js
+++ src/App.js
@@ -20,10 +20,10 @@ function App() {
   return (
     <>
       <div className="container mx-auto my-7">
+        <ImageSearch setTerm={setTerm} />
         {images.length === 0 && (
           <h1 className="text-5xl text-center mt-32">이미지가 없습니다.</h1>
         )}
-        <ImageSearch setTerm={setTerm} />
         <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
           {images.map((image) => (
             <ImageCard key={image.id} image={image} />
